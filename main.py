262e26e39748b89811b517cf3d9f0882ded1d19e
LiQIYi <ylwnpzzzn@163.com>
2017-11-26 19:43:06 +0800

    更新

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ from tools import voice_TO_literals # 声音转文字
 import mp3play
 import time
 
+WAV_PATH = r'.\file\demo.wav'
+MP3_PATH = r'.\file\test.mp3'
+
 def playMusic():
     # 播放mp3
-    play_mp3 = mp3play.load(r'.\file\test.mp3')  # 加载mp3文件
+    play_mp3 = mp3play.load(MP3_PATH)  # 加载mp3文件
     play_mp3.play()  # 开始播放mp3
     time.sleep(play_mp3.seconds() + 1)  # 播放"mp3的长度+1"秒
     play_mp3.stop()  # 播放完毕
@@ -23,16 +26,16 @@ if __name__ == "__main__":
     flag = True
     while flag:
         # 1.录音
-        print "请说话："
-        sound_recording.record_to_file(r'..\file\demo.wav')
-        print "录音结束,说'退出'结束程序"
+        print u"请说话："
+        sound_recording.record_to_file(WAV_PATH)
+        print u"录音结束,说'退出'结束程序"
         # 2.声音转文字 保存到say变量里去
-        say = voice_TO_literals.get_say()
+        say = voice_TO_literals.get_say(WAV_PATH)
         # print say
         # 3.机器人回答
         if '退出' not in say:
             # 把机器人回答转成语音
-            rbot.run(say,r'.\file\test.mp3')
+            rbot.run(say,MP3_PATH)
             # 播放语音
             playMusic()
         else:
