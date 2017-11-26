57e0b51a532355a04f02b9e6d578f2108ed5ec0b
LiQIYi <ylwnpzzzn@163.com>
2017-11-26 19:45:25 +0800

    更新

--- tools/literals_TO_voice.py
+++ tools/literals_TO_voice.py
@@ -13,15 +13,14 @@ APP_ID = '你的APP_ID'
 API_KEY = '你的API_KEY'
 SECRET_KEY = '你的SECRET_KEY'
 aipSpeech = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
-WAV = "test.mp3"
 
-def say(say):
+def say(say,WAV):
     """
     文字转语音
     :param say: 文字， 这里是机器人的回答
     :return:
     """
-    result  = aipSpeech.synthesis(say, 'zh', 1, {'vol': 5,'per':3}) # 百度api相关方法
+    result = aipSpeech.synthesis(say, 'zh', 1, {'vol': 5,'per':3}) # 百度api相关方法
     # 检查本目录下是否存在test.mp3文件,如果存在则删除,方便之后创建新的mp3文件
     if os.path.exists(WAV):
         os.remove(WAV)
